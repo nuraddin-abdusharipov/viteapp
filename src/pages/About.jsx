0ad09608ab5715cccdf33f919c86ff57c0fc203c
behruz <bmw999398@gmail.com>
2025-12-09 15:13:55 +0300

    uzgardi Menu.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
         initial={{ y: -40, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
       >
-        About Ourr Restaurant
+        About Our Restaurant
       </motion.h2>
 
       <motion.div
