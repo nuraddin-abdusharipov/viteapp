241ca9a139c5a0ff645f9ace2fbf77c2d5d6bb89
Ibratbek Shavkatov <ibratbek@example.com>
2025-12-09 17:12:27 +0500

    uzgardi Cart.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
         initial={{ y: -40, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
       >
-        About Ourrr Restaurant
+        About Ourr Restaurant
       </motion.h2>
 
       <motion.div
